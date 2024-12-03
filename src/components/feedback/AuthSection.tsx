a56148e69db22c62cbf7de741db238201a95b2e8
Farzad Shami <farzadshami845@gmail.com>
2024-12-03 09:26:56 +0100

    fix: fix the assets path for logo

--- src/components/feedback/AuthSection.tsx
+++ src/components/feedback/AuthSection.tsx
@@ -49,7 +49,7 @@ export const AuthSection = () => {
               className='w-full'
               onClick={() => socialLogin('google')}
             >
-              <img src='/google-icon.svg' className='w-5 h-5 mr-2' alt='Google' />
+              <img src='/static/google-icon.svg' className='w-5 h-5 mr-2' alt='Google' />
               Continue with Google
             </Button>
 
@@ -58,7 +58,7 @@ export const AuthSection = () => {
               className='w-full'
               onClick={() => socialLogin('orcid')}
             >
-              <img src='/orcid-icon.svg' className='w-5 h-5 mr-2' alt='ORCID' />
+              <img src='/static/orcid-icon.svg' className='w-5 h-5 mr-2' alt='ORCID' />
               Continue with ORCID
             </Button>
 
