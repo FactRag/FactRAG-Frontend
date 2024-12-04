ac738c0ebece81dd5cee20ea5378052d18d11f21
Farzad Shami <farzadshami845@gmail.com>
2024-12-04 12:38:53 +0100

    feat(Analytics): Add the Analytics component to our app

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,21 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { HomePage } from './pages/HomePage'
 import { ResultsPage } from './pages/ResultsPage'
 import { AuthProvider } from './contexts/AuthContext'
-
+import { Analytics } from '@vercel/analytics/react';
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/results" element={<ResultsPage />} />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+    <>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/results" element={<ResultsPage />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+      <Analytics />
+    </>
+
   )
 }
 
